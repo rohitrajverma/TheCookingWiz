68f52a547c179f4d7c591da79488fa04039f575f
rohitrajverma <rohitrajverma1611@gmail.com>
2020-09-13 16:17:11 +0530

    Updates

---
+++ precache-manifest.ab163106af8ecc159ecd83a10752f523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2babcf31f9f71d318ea06746b212aef1",
+    "url": "/TheCookingWiz/index.html"
+  },
+  {
+    "revision": "82782f34fe9e84e9ce41",
+    "url": "/TheCookingWiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "716b38f99d1e9d4d30c1",
+    "url": "/TheCookingWiz/static/js/2.376f663c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheCookingWiz/static/js/2.376f663c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82782f34fe9e84e9ce41",
+    "url": "/TheCookingWiz/static/js/main.6274e3fa.chunk.js"
+  },
+  {
+    "revision": "bd197da6bb50aa6a9f49",
+    "url": "/TheCookingWiz/static/js/runtime-main.913cd582.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TheCookingWiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
